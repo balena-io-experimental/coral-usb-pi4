b832f85b34ef5d7672938009a78a04ec7c5f90e2
Daniel Borek <szony@users.noreply.github.com>
2020-04-01 14:19:09 +0100

    Added opencv and removed stuff from pip install

--- edge/run.sh
+++ edge/run.sh
@@ -2,8 +2,8 @@
 
 echo "Starting..."
 
-python3 src/web_streaming_classify.py \
-        --model models/inception_v2_224_quant_edgetpu.tflite \
-        --label models/imagenet_labels.txt
+#python3 src/web_streaming_classify.py \
+#        --model models/inception_v2_224_quant_edgetpu.tflite \
+#        --label models/imagenet_labels.txt
 
-sleep infinity
+python3 examples/lite/examples/image_classification/raspberry_pi/classify_picamera.py
